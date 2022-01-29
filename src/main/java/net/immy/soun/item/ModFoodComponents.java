04997f8e8ec4759e32841258dc2d0986fa4cb29a
immy soun <immansounthon@gmail.com>
2022-01-29 16:54:56 +0000

    heart fragments
    added heart fragments and heart crafting recipe

---
+++ src/main/java/net/immy/soun/item/ModFoodComponents.java
@@ -0,0 +1,8 @@
+package net.immy.soun.item;
+
+import net.minecraft.item.FoodComponent;
+
+public class ModFoodComponents {
+
+    public static final FoodComponent HEART = new FoodComponent.Builder().hunger(0).saturationModifier(0.0f).build();
+}
--- src/main/java/net/immy/soun/item/ModItems.java
+++ src/main/java/net/immy/soun/item/ModItems.java
@@ -11,6 +11,17 @@ public class ModItems {
 
     public static final Item NOB = registerItem("token", new Item(new FabricItemSettings().group(ItemGroup.MISC)));
 
+    public static final Item SAD_FRAGMENT = registerItem("sadfrag", new Item(new FabricItemSettings().group(ItemGroup.MISC)));
+    public static final Item HAPPY_FRAGMENT = registerItem("happyfrag", new Item(new FabricItemSettings().group(ItemGroup.MISC)));
+    public static final Item ANGRY_FRAGMENT = registerItem("angyfrag", new Item(new FabricItemSettings().group(ItemGroup.MISC)));
+    public static final Item FEAR_FRAGMENT = registerItem("pussio", new Item(new FabricItemSettings().group(ItemGroup.MISC)));
+    public static final Item SURPRISED_FRAGMENT = registerItem("omigaa", new Item(new FabricItemSettings().group(ItemGroup.MISC)));
+    public static final Item EW_FRAGMENT = registerItem("ewfrag", new Item(new FabricItemSettings().group(ItemGroup.MISC)));
+    public static final Item EMPTY_FRAGMENT = registerItem("nulledfrag", new Item(new FabricItemSettings().group(ItemGroup.MISC)));
+    public static final Item ENLIGHTENED_FRAGMENT = registerItem("wheelfrag", new Item(new FabricItemSettings().group(ItemGroup.MISC)));
+
+    public static final Item HEART = registerItem("heart", new Item(new FabricItemSettings().group(ItemGroup.FOOD).food(ModFoodComponents.HEART)));
+
     private static Item registerItem(String name, Item item)
     {
         return Registry.register(Registry.ITEM, new Identifier(sounMod.MOD_ID, name), item);
